1b74b3c411e46aade1693f3a83688fe0e445835d
HarrisonFah <fah.harrison@gmail.com>
2020-10-19 20:07:38 -0600

    Added firestore dependencies

--- Bookworm/app/src/main/java/com/example/bookworm/MainActivity.java
+++ Bookworm/app/src/main/java/com/example/bookworm/MainActivity.java
@@ -4,11 +4,19 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.firestore.FirebaseFirestore;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FirebaseFirestore db;
+
+        
+
+
     }
 }
