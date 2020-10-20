c4cf808592925abaecdb91337d2bf403b55de79a
HarrisonFah <fah.harrison@gmail.com>
2020-10-20 16:46:34 -0600

    Added User and Request classes

--- Bookworm/app/src/main/java/com/example/bookworm/Book.java
+++ Bookworm/app/src/main/java/com/example/bookworm/Book.java
@@ -22,6 +22,11 @@ public class Book {
     }
 
     /* Create Book object with minimal parameters */
+    public Book (String title, String author, String isbn){
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+    }
 
 
     /**
