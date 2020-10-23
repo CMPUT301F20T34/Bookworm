ec9aa5af7b6517e6060c0dd179c3980988f64583
Paul Saunders <psaunder@ualberta.ca>
2020-10-22 21:27:58 -0600

    Added skeleton code to UI test

--- Bookworm/app/src/androidTest/java/com/example/bookworm/SignUpActivityTest.java
+++ Bookworm/app/src/androidTest/java/com/example/bookworm/SignUpActivityTest.java
@@ -1,4 +1,55 @@
-package com.example.bookworm;
+package com.example.simpleparadox.listycity;
 
+import android.app.Activity;
+
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.rule.ActivityTestRule;
+
+import com.example.bookworm.MainActivity;
+import com.robotium.solo.Solo;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+/**
+ * Test class for MainActivity. All the UI tests are written here.
+ * Robotium test framework is used
+ */
 public class SignUpActivityTest {
+    private Solo solo; // Main test class of robotium
+
+    @Rule
+    public ActivityTestRule<MainActivity> rule =
+            new ActivityTestRule<>(MainActivity.class, true, true);
+
+    /**
+     * Runs before all tests and creates solo instance
+     * @throws Exception
+     */
+    @Before
+    public void setUp() throws Exception {
+        solo = new Solo(InstrumentationRegistry.getInstrumentation(), rule.getActivity());
+    }
+
+    /**
+     * Gets the activity
+     * @throws Exception
+     */
+    @Test
+    public void start() throws Exception {
+        Activity activity = rule.getActivity();
+    }
+
+
+
+    /**
+     * Closes the activity after each test.
+     * @throws Exception
+     */
+    @After
+    public void tearDown() throws Exception {
+        solo.finishOpenedActivities();
+    }
 }
