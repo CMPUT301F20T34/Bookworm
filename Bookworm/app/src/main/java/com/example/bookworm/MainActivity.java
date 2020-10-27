9aba830200746834342d7d9fb09b241abaec4885
HarrisonFah <fah.harrison@gmail.com>
2020-10-26 20:15:12 -0600

    Set up the database class and created a test for it.

--- Bookworm/app/src/main/java/com/example/bookworm/MainActivity.java
+++ Bookworm/app/src/main/java/com/example/bookworm/MainActivity.java
@@ -1,12 +1,14 @@
 package com.example.bookworm;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.text.style.LineBackgroundSpan;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnFailureListener;
@@ -23,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
     Library exampleLibrary;
     String TAG = "Sample";
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,43 +37,15 @@ public class MainActivity extends AppCompatActivity {
         Book exampleBook = new Book("Harry Potter", "J.K Rowling", "Available");
         Owner exampleOwner = new Owner("Mike", "hunter2", "mike@hotmail.com", "592-441-0235");
         Borrower exampleBorrower = new Borrower(exampleOwner);
+        Request exampleReq = new Request(exampleBook, exampleBorrower, "Status");
         exampleLibrary = new Library();
         exampleLibrary.addOwner(exampleOwner);
         exampleLibrary.addBorrower(exampleBorrower);
         exampleLibrary.addBook(exampleBook);
+        exampleLibrary.addRequest(exampleReq);
 
-        final CollectionReference libraryCollection = db.collection("libraries");
-
-        libraryCollection
-                .document("example")
-                .set(exampleLibrary)
-                .addOnSuccessListener(new OnSuccessListener<Void>() {
-                    @Override
-                    public void onSuccess(Void aVoid) {
-                        // These are a method which gets executed when the task is succeeded
-                        //Log.d(TAG, "Data has been added successfully!");
-                    }
-                })
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        // These are a method which gets executed if there’s any problem
-                        //Log.d(TAG, "Data could not be added!" + e.toString());
-                    }
-                });
-
-        libraryCollection.addSnapshotListener(new EventListener<QuerySnapshot>() {
-            @Override
-            public void onEvent(@Nullable QuerySnapshot queryDocumentSnapshots, @Nullable
-                    FirebaseFirestoreException error) {
-                // Clear the old list
-                for(QueryDocumentSnapshot doc: queryDocumentSnapshots)
-                {
-                    //Log.d(TAG, String.valueOf(doc.getData()));
-                    exampleLibrary = doc.toObject(Library.class);
-                }
-            }
-        });
+        Database.createListener(exampleLibrary);
+        Database.writeLibrary(exampleLibrary);
 
         Log.d(TAG, String.valueOf(exampleLibrary.getBooks()));
 
