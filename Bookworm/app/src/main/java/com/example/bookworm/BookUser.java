531abb677661fb1fc23cbde632e02e5e2ec46155
HarrisonFah <fah.harrison@gmail.com>
2020-10-28 15:55:56 -0600

    Clearing app file so I can get a fresh install since my gradle files are messed up

--- Bookworm/app/src/main/java/com/example/bookworm/BookUser.java
+++
@@ -1,9 +0,0 @@
-package com.example.bookworm;
-
-import java.util.ArrayList;
-
-public interface BookUser {
-    public void addBook(Book book);
-    public ArrayList<Book> getBooks();
-    public void deleteBook(Book book);
-}
