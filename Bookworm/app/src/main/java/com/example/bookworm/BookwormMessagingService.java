799b95818be378ba8788ac9ecb9d85a5d76a0047
Paul Saunders <psaunder@ualberta.ca>
2020-11-14 22:16:14 -0700

    Notifications for request creations work only on SearchResultsActivity

--- Bookworm/app/src/main/java/com/example/bookworm/BookwormMessagingService.java
+++ Bookworm/app/src/main/java/com/example/bookworm/BookwormMessagingService.java
@@ -1,6 +1,14 @@
 package com.example.bookworm;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.util.Log;
+
+import androidx.core.app.NotificationCompat;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
@@ -15,18 +23,48 @@ public class BookwormMessagingService extends FirebaseMessagingService {
         // TODO(developer): Handle FCM messages here.
         // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
         Log.d(TAG, "From: " + remoteMessage.getFrom());
-
         // Check if message contains a data payload.
         if (remoteMessage.getData().size() > 0) {
             Log.d(TAG, "Message data payload: " + remoteMessage.getData());
+
         }
 
         // Check if message contains a notification payload.
         if (remoteMessage.getNotification() != null) {
             Log.d(TAG, "Message Notification Body: " + remoteMessage.getNotification().getBody());
+            sendNotification(remoteMessage.getNotification(), App.CHANNEL_ID_1);
         }
 
         // Also if you intend on generating your own notifications as a result of a received FCM
         // message, here is where that should be initiated. See sendNotification method below.
     }
+
+    /**
+     * Posts a notification to a specific channel
+     * From: https://stackoverflow.com/questions/37351354/firebase-cloud-messaging-notification-not-received-by-device
+     * Author: xec86
+     * Date Accessed: November 14th, 2020
+     * @param notification the content of the notification
+     * @param channel Represents the channel that the notification is displayed on.
+     */
+    private void sendNotification(RemoteMessage.Notification notification, String channel) {
+        Intent intent = new Intent(this, SearchResultsActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
+            PendingIntent.FLAG_ONE_SHOT);
+
+        Uri defaultSoundUri= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, channel)
+            .setSmallIcon(R.drawable.ic_baseline_message_1)
+            .setContentTitle(notification.getTitle())
+            .setContentText(notification.getBody())
+            .setSound(defaultSoundUri)
+            .setContentIntent(pendingIntent);
+
+        NotificationManager notificationManager =
+            (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        notificationManager.notify(0 /* ID of notification */, notificationBuilder.build());
+    }
+
 }
--- firestore/functions/index.js
+++ firestore/functions/index.js
@@ -1,7 +1,8 @@
+"use strict";
+
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
-admin.initializeApp();
-"use strict";
+admin.initializeApp(functions.config().firebase);
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
@@ -9,29 +10,33 @@ admin.initializeApp();
 
 exports.sendRequestNotification = functions.firestore.document("/Libraries/{libraryName}/requests/{requestID}")
     .onCreate(async (snap, context) => {
+        // Get necessary variables
         const libraryName = context.params.libraryName;
-
         const data = snap.data();
         const owner = data.book.owner;
         const title = data.book.title;
         const creator = data.creator.username;
 
-        const tokenSnapshot = await admin.database().ref(`/Libraries/${libraryName}/users/${owner}`).once('value');
+        // Get registration token for user
+        const userSnapshot = await admin.firestore().doc(`/Libraries/${libraryName}/users/${owner}`).get();
+        const token = userSnapshot.data().registrationToken;
+        if (token === null) {
+            return null;
+        }
 
-        const token = tokenSnapshot.data().registrationToken;
-
+        // Create message
         const message = {
             notification: {
               title: `A request has been made for ${title}`,
-              body: `${creator} requested the book ${title}. Please respond as soon as possible.`,
+              body: `User "${creator}" has requested the book "${title}".`,
             },
         };
         
+        // Send message, wait for errors
         const response = await admin.messaging().sendToDevice(token, message)
         response.results.forEach((result, index) => {
-            const error = result.error;
-            if (error) {
-              console.error('Failure sending notification to', token, error);
+            if (result.error) {
+              console.error('Failure sending notification to', token, result.error);
             }
         });
     })
