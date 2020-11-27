e06147c2348069ee5a8b3bda8ce72682ef105818
yuheng_shi <yuheng5@ualberta.ca>
2020-11-27 12:17:23 -0700

    modified:   Bookworm/app/src/androidTest/java/com/example/bookworm/AcceptDeclineRequestTest.java 	modified:   Bookworm/app/src/androidTest/java/com/example/bookworm/BorrowerMainActivityTest.java 	modified:   Bookworm/app/src/androidTest/java/com/example/bookworm/OwnerMapTest.java 	modified:   Bookworm/app/src/main/java/com/example/bookworm/Database.java 	modified:   Bookworm/app/src/main/res/layout/activity_owner_booklist.xml

--- Bookworm/app/src/androidTest/java/com/example/bookworm/OwnerMapTest.java
+++ Bookworm/app/src/androidTest/java/com/example/bookworm/OwnerMapTest.java
@@ -16,7 +16,10 @@ import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.rule.ActivityTestRule;
 
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 import com.robotium.solo.Solo;
 
 import org.junit.After;
@@ -26,6 +29,7 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertTrue;
 
 public class OwnerMapTest {
@@ -33,11 +37,19 @@ public class OwnerMapTest {
     private FirebaseAuth fAuth;
 
     @Rule
-    public ActivityTestRule<MainActivity> rule =
-            new ActivityTestRule<>(MainActivity.class, true, true);
+    public ActivityTestRule<LoginActivity> rule =
+            new ActivityTestRule<>(LoginActivity.class, true, true);
 
-    public Book mockBook() {
-        return new Book("Title", "author", "description", "isbn", "accepted");
+    private User MockUser() {
+        return new User("Steve", "1234567", "steve@gmail.com", "1234567890");
+    }
+
+    public Book MockBook() {
+        return new Book("Title", "author", "description", "isbn", "available");
+    }
+
+    private Request MockRequest() {
+        return new Request(MockBook(), MockUser(), "available");
     }
 
     /**
@@ -50,47 +62,84 @@ public class OwnerMapTest {
         fAuth = FirebaseAuth.getInstance();
         fAuth.signOut();
         solo = new Solo(InstrumentationRegistry.getInstrumentation(), rule.getActivity());
-        Activity activity = rule.getActivity();
-        Intent intent = new Intent(activity.getApplicationContext(), LoginActivity.class);
-        activity.startActivity(intent);
+        //perform login
         solo.assertCurrentActivity("Wrong Activity", LoginActivity.class);
         solo.enterText((EditText) solo.getView(R.id.username_login), "muligan");
         solo.enterText((EditText) solo.getView(R.id.password_login), "123456");
         solo.clickOnButton("LOGIN");
-        Intent intent2 = new Intent(activity.getApplicationContext(), OwnerMapActivity.class);
-        activity.startActivity(intent2);
-        solo.assertCurrentActivity("Wrong Activity", OwnerMapActivity.class);
-//        solo.enterText((EditText) solo.getView(R.id.username_login), "muligan");
-//        solo.enterText((EditText) solo.getView(R.id.password_login), "123456");
-//        solo.clickOnButton("LOGIN");
-//        //solo.assertCurrentActivity("Wrong Activity", MainActivity.class);
-//        solo.clickOnButton("MY BOOKLIST");
-//        solo.assertCurrentActivity("Wrong Activity", OwnerBooklistActivity.class);
+        solo.assertCurrentActivity("Wrong Activity", MainActivity.class);
     }
+
 
     /**
-     * Gets the Activity
-     *
-     * @throws Exception
+     * Tests the functionality of the owner map
+     * @throws InterruptedException Sleeping thread is interrupted
      */
     @Test
-    public void start() throws Exception {
-        Activity activity = rule.getActivity();
-    }
-
-    @Test
-    public void MapTest() throws InterruptedException {
-        LatLng edmonton = new LatLng(53.52, -113.52);
-        Book book = mockBook();
+    public void OwnerMapTest() throws InterruptedException {
+        //LatLng edmonton = new LatLng(53.52, -113.52);
+        Book book = MockBook();
         book.setOwnerId(fAuth.getCurrentUser().getUid());
-        Database.writeBook(book);
+        Database.writeBookSynchronous(book);
         while (Database.getListenerSignal() == 0) {
             Thread.sleep(100);
         }
-
+        Request request = MockRequest();
+        Database.createSynchronousRequest(request);
+        while (Database.getListenerSignal() == 0) {
+            Thread.sleep(100);
+        }
+        clickOnRequest();
+        //int views = solo.getCurrentViews().size();
+        solo.clickOnView(solo.getView(R.id.accept_decline_request_accept_button));
+        solo.assertCurrentActivity("Wrong Activity", OwnerMapActivity.class);
+        TextView info = (TextView) solo.getView(R.id.location_info);
+        String LocInfo = info.getText().toString();
         solo.clickOnButton("CONFIRM");
-        assertEquals(edmonton.latitude, book.getLocation().latitude, 0.0);
-        assertEquals(edmonton.longitude, book.getLocation().longitude, 0.0);
+        solo.goBack();
+
+        Database.getAcceptedRequest()
+                .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                    @Override
+                    public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                        for (QueryDocumentSnapshot doc : queryDocumentSnapshots) {
+                            //convert the QuerySnapshot to the book object
+                            Request request = doc.toObject(Request.class);
+                            //check if the status of the book is "Accepted"
+                            assertEquals(53.52,request.getLat(),0);
+                            assertEquals(-113.52,request.getLng(),0);
+                        }
+                    }
+                });
+
+       Database.deleteBookSynchronous(MockBook());
+       Database.deleteRequest(MockRequest());
+    }
+
+    /**
+     * Handles all actions related to viewing and clicking on
+     * the request previously made.
+     */
+    private void clickOnRequest() {
+        // Go to the book where the requests were made
+        solo.clickOnView(solo.getView(R.id.booklist_button));
+        solo.assertCurrentActivity("Wrong Activity", OwnerBooklistActivity.class);
+        solo.clickOnText(MockBook().getTitle());
+
+        // Go to the list of requests for the book
+        solo.assertCurrentActivity("Wrong Activity", EditBookActivity.class);
+        solo.clickOnView(solo.getView(R.id.button10));
+        solo.assertCurrentActivity("Wrong Activity", ViewRequestsActivity.class);
+
+        // Click on the request that was just made
+        assertTrue(solo.waitForText(MockUser().getUsername(), 1, 1000));
+        solo.clickInRecyclerView(0);
+        solo.assertCurrentActivity("Wrong Activity", AcceptDeclineRequestActivity.class);
+
+//        // Check that it fetches account of person that made request
+//        assertTrue(solo.waitForText("pahasa", 1, 1000));
+//        assertTrue(solo.waitForText("psaunder@ualberta.ca", 1, 1000));
+//        assertTrue(solo.waitForText("7802467244", 1, 1000));
     }
 
     /**
